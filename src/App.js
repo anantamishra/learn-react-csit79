348db9b8d803efb0e0c349e3b33a4ada3c8fa32c
anantamishra <mishraananta2017@gmail.com>
2024-07-07 12:33:37 +0545

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+  const [result, setResult] = useState('');
+
+  function handleChange1(e) {
+    setValue1(e.target.value);
+  }
+  function handleChange2(e) {
+    setValue2(e.target.value);
+  }
+  function add() {
+    setResult(Number(value1) + Number(value2))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex w-full min-h-screen justify-center items-center flex-col gap-5'>
+      <div className='flex gap-5'>
+        <input value={value1} onChange={handleChange1} className='border-black border-2 rounded px-4 py-2' />
+        <input value={value2} onChange={handleChange2} className='border-black border-2 rounded px-4 py-2' />
+      </div>
+      <button onClick={add} className='px-4 py-2 bg-blue-600 rounded text-white'>
+        Add
+      </button>
+      <p>{result}</p>
     </div>
   );
 }
 
-export default App;
