2758a0b8205b9ff273b0092f32592675597133fc
anantamishra <mishraananta2017@gmail.com>
2024-07-11 12:18:00 +0545

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./home";
-import Blogs from "./blogs";
+import { useState, createContext, useContext } from "react";
 
-export default function App() {
+const UserContext = createContext();
+
+function App() {
+  const [user, setUser] = useState("Jesse Hall");
+
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="/blogs" element={<Blogs />} />
-    </Routes >
+    <UserContext.Provider value={user}>
+      <h1>Main:{`Hello ${user}!`}</h1>
+      <Component2 />
+    </UserContext.Provider>
   );
 }
+
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+      <button className="border border-2 bg-red-500 text-white text-lg p-2">Click me</button>
+    </>
+  );
+}
+export default App;
