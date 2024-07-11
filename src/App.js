fc7fecdcad876840fad306ca63063ba42eeae00f
anantamishra <mishraananta2017@gmail.com>
2024-07-11 12:00:26 +0545

    init

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./home";
+import Blogs from "./blogs";
 
-function App() {
-  const [activeButton, setActiveButton] = useState(null);
-  const [value, setValue] = useState(0);
-
-  const handleClick = (buttonId) => {
-    setActiveButton(buttonId);
-    if (activeButton === "add") {
-      setValue(value + 1);
-    }
-    else if (activeButton === "subtract") {
-      setValue(value - 1);
-    }
-  };
-
+export default function App() {
   return (
-    <div className="justify-center flex items-center min-h-screen w-full gap-5">
-      <button
-        className={` px-5 py-4 rounded-lg ${activeButton === 'add' ? 'bg-red-500' : 'bg-emerald-300'}`}
-        onClick={() => handleClick('add')}
-      >
-        Add
-      </button>
-      <button
-        className={` px-5 py-4 rounded-lg ${activeButton === 'subtract' ? 'bg-red-500' : 'bg-emerald-300'}`}
-        onClick={() => handleClick('subtract')}
-      >
-        Subtract
-      </button>
-      <h1>{value}</h1>
-    </div>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/blogs" element={<Blogs />} />
+    </Routes >
   );
 }
-
-export default App;
