aa7cf2138ba0ace8f3be0c5ffcca62dabef35c25
anantamishra <mishraananta2017@gmail.com>
2024-07-16 12:13:00 +0545

    init

--- src/todo.js
+++ src/todo.js
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './App.css';
 
-export default function TODO() {
+function TODO() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    const addTask = () => {
+        if (newTask.trim()) {
+            setTasks([...tasks, { text: newTask, completed: false }]);
+            setNewTask('');
+        }
+    };
+
+    const toggleTask = (index) => {
+        const updatedTasks = tasks.map((task, i) =>
+            i === index ? { ...task, completed: !task.completed } : task
+        );
+        setTasks(updatedTasks);
+    };
+
     return (
         <div className='flex min-h-screen w-full justify-center items-center flex-col'>
             <div className='p-10 shadow rounded border flex flex-col gap-2'>
                 <h1 className='font-bold'>TO DOs</h1>
                 <div className='flex gap-2'>
-                    <input type="text" placeholder="Add to item" className='border border-black p-2 rounded' />
-                    <button className=' bg-red-500 rounded-sm text-white font-bold h-10 w-10 flex justify-center items-center'>+</button>
+                    <input
+                        type="text"
+                        placeholder="Add to item"
+                        className='border border-black p-2 rounded'
+                        value={newTask}
+                        onChange={(e) => setNewTask(e.target.value)}
+                    />
+                    <button
+                        className='bg-red-500 rounded-sm text-white font-bold h-10 w-10 flex justify-center items-center'
+                        onClick={addTask}
+                    >
+                        +
+                    </button>
                 </div>
-                <div>
-                    <input type="checkbox" id="input1" name="input1" value="Car" />
-                    <label for="input1"> I have a car</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="input1" name="input1" value="Car" />
-                    <label for="input1" className='line-through'> I have a car</label>
+                <div className='mt-4'>
+                    {tasks.map((task, index) => (
+                        <div key={index} className='flex items-center gap-2'>
+                            <input
+                                type="checkbox"
+                                id={`task-${index}`}
+                                checked={task.completed}
+                                onChange={() => toggleTask(index)}
+                            />
+                            <label
+                                htmlFor={`task-${index}`}
+                                className={task.completed ? 'line-through' : ''}
+                            >
+                                {task.text}
+                            </label>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
+export default TODO;
