8887a3adf680d33c8fd58c4ca546aaa9af3c28ad
anantamishra <mishraananta2017@gmail.com>
2024-07-18 16:04:31 +0545

    updated UI of Calculator

--- src/calculator.js
+++ src/calculator.js
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
 
 function CalculatorApp() {
-    return (
-        <div className="flex items-center w-full justify-center h-screen bg-white">
-            <Calculator />
-        </div>
-    );
-}
-
-export default CalculatorApp;
-
-
-
-export const Calculator = () => {
     const [input, setInput] = useState('');
     const [result, setResult] = useState('');
+    const buttonItems = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '%', '+', 'C', '='];
 
     const handleButtonClick = (value) => {
         if (value === '=') {
@@ -40,27 +29,28 @@ export const Calculator = () => {
     };
 
     return (
-        <div className=" w-1/2 bg-gradient-to-br from-purple-400 to-orange-300 p-6 rounded-lg shadow-lg">
-            <div className="display bg-white p-4 rounded-lg text-right mb-4">
-                <div className="text-gray-600 text-sm py-10">{input}</div>
-                <div className="text-4xl font-bold">{result}</div>
-            </div>
-            <div className="buttons grid grid-cols-4 gap-2">
-                {['7', '8', '9', '/'].map((value) => (
-                    <button key={value} className="button" onClick={() => handleButtonClick(value)}>{value}</button>
-                ))}
-                {['4', '5', '6', '*'].map((value) => (
-                    <button key={value} className="button" onClick={() => handleButtonClick(value)}>{value}</button>
-                ))}
-                {['1', '2', '3', '-'].map((value) => (
-                    <button key={value} className="button" onClick={() => handleButtonClick(value)}>{value}</button>
-                ))}
-                {['0', '.', '=', '+'].map((value) => (
-                    <button key={value} className="button col-span-1" onClick={() => handleButtonClick(value)}>{value}</button>
-                ))}
-                <button className="button col-span-2" onClick={() => handleButtonClick('C')}>C</button>
-                <button className="button col-span-2" onClick={() => handleButtonClick('%')}>%</button>
+        <div className="flex items-center w-full justify-center h-screen bg-[#0f172a] flex-col gap-5">
+            <p className='text-xl font-bold text-white'>Simple Calculator App</p>
+            <div className="w-1/2 bg-gradient-to-br from-purple-400 to-orange-300 p-6 rounded-lg shadow-lg">
+                <div className="bg-[#0f172a] p-4 rounded-lg text-right mb-4 text-white">
+                    <input className="caret-[#0f172a] bg-inherit text-right" disabled value={input} />
+                    <input className="text-4xl font-bold caret-[#0f172a] bg-inherit text-right" disabled value={result} />
+                </div>
+                <div className="buttons grid grid-cols-4 gap-2">
+                    {buttonItems.map((value, index) => (
+                        <button
+                            key={value}
+                            className={`bg-[#0f172a] p-4 rounded-lg shadow-md text-xl font-semibold text-white hover:bg-[#383d4b] ${index >= buttonItems.length - 2 ? 'col-span-2' : ''}`}
+                            onClick={() => handleButtonClick(value)}
+                        >
+                            {value}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     );
-};
+}
+
+export default CalculatorApp;
+
