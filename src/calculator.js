9cdf5e78d53a9a16a7f4162c7e13f65a40df6127
anantamishra <mishraananta2017@gmail.com>
2024-07-19 12:02:47 +0545

    minor fix

--- src/calculator.js
+++ src/calculator.js
@@ -32,7 +32,7 @@ function CalculatorApp() {
         <div className="flex items-center w-full justify-center h-screen bg-[#0f172a] flex-col gap-5">
             <p className='text-xl font-bold text-white'>Simple Calculator App</p>
             <div className="w-1/2 bg-gradient-to-br from-purple-400 to-orange-300 p-6 rounded-lg shadow-lg">
-                <div className="bg-[#0f172a] p-4 rounded-lg text-right mb-4 text-white">
+                <div className="bg-[#0f172a] p-4 rounded-lg text-right mb-4 text-white flex flex-col">
                     <input className="caret-[#0f172a] bg-inherit text-right" disabled value={input} />
                     <input className="text-4xl font-bold caret-[#0f172a] bg-inherit text-right" disabled value={result} />
                 </div>
