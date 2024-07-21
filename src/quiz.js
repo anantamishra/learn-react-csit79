4256ed2ee266b4ce587636b3e104800185968cbc
anantamishra <mishraananta2017@gmail.com>
2024-07-21 10:18:29 +0545

    quiz app

--- src/quiz.js
+++ src/quiz.js
@@ -1,20 +1,28 @@
-import React, { useEffect, useState } from 'react';
 
-function QuizApp() {
-    let [dogImage, setDogImage] = useState("")
-    const dataUrl = "https://dog.ceo/api/breeds/image/random"
+import { useEffect, useState } from "react";
+
+export default function JokePage() {
+    const [joke, setJoke] = useState("");
+    const url = "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple";
+
 
     useEffect(() => {
-        fetch(dataUrl)
-            .then(response => response.json())
-            .then(data => setDogImage(data.message))
-    }, [])
+        async function fetchJoke() {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error("Failed to fetch quiz");
+                }
+                const quizData = await response.json();
+                console.log(quizData.results);
+                setJoke(quizData.value);
+            } catch (error) {
+                console.error("Error fetching quiz:", error);
+            }
+        }
 
-    return (
-        <div>
-            {dogImage && <img src={dogImage}></img>}
-        </div>
-    );
+        fetchJoke();
+    }, []);
+
+    return <div>{joke}</div>
 }
-
-export default QuizApp;
