b04dafbae362b06beec633b34826d972fceee52c
anantamishra <mishraananta2017@gmail.com>
2024-07-21 12:37:35 +0545

    init

--- src/quiz.js
+++ src/quiz.js
@@ -1,28 +1,36 @@
 
 import { useEffect, useState } from "react";
 
-export default function JokePage() {
-    const [joke, setJoke] = useState("");
+export default function QuizApp() {
+    const [quizs, setQuizs] = useState([]);
     const url = "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple";
 
 
     useEffect(() => {
-        async function fetchJoke() {
+        async function fetchQuiz() {
             try {
                 const response = await fetch(url);
+                console.log("response is:", response);
                 if (!response.ok) {
                     throw new Error("Failed to fetch quiz");
                 }
                 const quizData = await response.json();
-                console.log(quizData.results);
-                setJoke(quizData.value);
+                setQuizs(quizData.results);
             } catch (error) {
                 console.error("Error fetching quiz:", error);
             }
         }
 
-        fetchJoke();
+        fetchQuiz();
     }, []);
 
-    return <div>{joke}</div>
+    return <div>
+        {quizs.map((quiz, index) => (
+            <div key={index}>
+                <h3>Question: {quiz.question}</h3>
+                <h3>Correct Answer: {quiz.correct_answer}</h3>
+                <h3>InCorrect Answer: {quiz.incorrect_answer}</h3>
+            </div>
+        ))}
+    </div>
 }
