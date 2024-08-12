211e73c43bc9f2a6bc2fc0230a1c19930371db61
anantamishra <mishraananta2017@gmail.com>
2024-08-12 12:16:59 +0545

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import logo from "../assets/staricon.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="flex w-full justify-between shadow px-20 py-5">
+      {/* <img src={logo} alt="" /> */}
       <h2 className="font-bold text-xl">Ananta Mishra</h2>
-      <ul className="flex gap-5 text-lg">
+      <ul className="flex gap-5 text-lg font-semibold">
         <li>Home</li>
         <li>About</li>
-        <li>Portfolio</li>
+        <Link to="/portfolio">
+          <li>Portfolio</li>
+        </Link>
         <li>Services</li>
       </ul>
       <button className="font-semibold text-lg bg-red-500 px-5 py-2 rounded-full text-white">
